e515580df71f6fc5f999ccb4187db9a980e6a2f6
Bruno Estevam <67297226+BrunoEstevamRJ@users.noreply.github.com>
2021-01-13 17:02:47 -0300

    Update views.py

--- mywebsite/base/views.py
+++ mywebsite/base/views.py
@@ -1,9 +1,25 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Cliente
 
-# Create your views here.
+# Criar views e models do contato
+
 
 def home(request):
     return render(request, 'base/home.html')
 
 def contato(request):
-    return render(request, 'base/contato.html')
+    if request.method=='POST':
+        cliente=Cliente()
+        name=request.POST.get('name')
+        telefone=request.POST.get('telefone')
+        email=request.POST.get('email')
+        menssagem=request.POST.get('menssagem')
+        cliente.nome=name
+        cliente.telefone=telefone
+        cliente.email=email
+        cliente.menssagem=menssagem
+        cliente.save()
+        # return HttpResponse('Obrigado pelo Contato')
+
+    return render(request,'base/contato.html')
